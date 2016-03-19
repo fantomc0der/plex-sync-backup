201fcdf5d46024f5663cf799b2d2c1d8a2a76eba
Mike Weisz <michael.w.weisz@gmail.com>
2016-03-19 17:03:33 -0400

    Refactoring names in mirror script. Outputting separate logs per drive sync instead of one master log file.

--- plex-drive-mirror.bat
+++ plex-drive-mirror.bat
@@ -4,19 +4,19 @@
 :: Make sure that any USB drives are explicitly assigned drive letters so after reconnection this script wont target an incorrect path
 
 :: Ensure sync log directory exists
-IF NOT EXIST "C:\sync_logs" MKDIR C:\sync_logs
+IF NOT EXIST "C:\plex_sync_logs" MKDIR C:\plex_sync_logs
 
 :: Sync each media drive to its backup drive
-CALL :SYNC_DIRECTORY "Anime" "E:\" "R:\"
-CALL :SYNC_DIRECTORY "Movies" "F:\" "S:\"
-CALL :SYNC_DIRECTORY "Television" "G:\" "T:\"
+CALL :SYNC_DRIVE "Anime" "E:\" "R:\"
+CALL :SYNC_DRIVE "Movies" "F:\" "S:\"
+CALL :SYNC_DRIVE "Television" "G:\" "T:\"
 
 :: Exit from main logic
 EXIT /B %ERRORLEVEL%
 
-:: Function that syncs a source directory to a destination backup directory, ignoring any root directory prefixed with "_temp".
+:: Function that syncs a source drive to a destination backup drive, ignoring any root directories prefixed with "_temp".
 :: (arguments: Name, SourceDirectory, DestinationDirectory)
-:SYNC_DIRECTORY
-@ECHO Starting sync for %~1 (%date% %time%)... >> C:\sync_logs\log.txt
-ROBOCOPY %~2 %~3 /MIR /XJD /MT:4 /R:10 /W:60 /XA:SHT /log+:C:\sync_logs\log.txt /XD _temp* $* "System Volume Information*" /XF *.dat *.sys *.db *.log
+:SYNC_DRIVE
+@ECHO Starting sync for %~1 (%date% %time%)... >> C:\plex_sync_logs\%~1%-log.txt
+ROBOCOPY %~2 %~3 /MIR /XJD /MT:4 /R:10 /W:60 /XA:SHT /log+:C:\plex_sync_logs\%~1%-log.txt /XD _temp* $* "System Volume Information*" /XF *.dat *.sys *.db *.log
 EXIT /B 0
